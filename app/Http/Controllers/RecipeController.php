4f6be3e66d61493372ed6c73fbeec2568eb738d1
Karlos Fernandes <karlospaiva@outlook.com>
2023-01-15 18:12:14 -0300

    update

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,10 +6,22 @@ use Illuminate\Support\Facades\Http;
 
 class RecipeController
 {
+    //Seafood
     public function index()
     {
-        $response = Http::get('www.themealdb.com/api/json/v1/1/lookup.php?i=52773');
+        $response = Http::get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
         $recipes = json_decode($response);
         return view('blog.blog-post')->with('recipes', $recipes);
     }
+
+}
+class RecipeController{
+     //Dessert
+     public function index()
+     {
+         $response = Http::get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert');
+         $recipes = json_decode($response);
+         return view('blog.blog-post')->with('recipes', $recipes);
+     }
+ 
 }
